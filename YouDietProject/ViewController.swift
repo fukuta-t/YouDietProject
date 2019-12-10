5524c351cf642970de20b349693689e6f5c39798
fukutappe <fukutappe@fukudatakuya-no-MacBook-Air.local>
2019-12-10 23:49:25 +0900

    プロジェクト作成、gitignoreの設定

---
+++ YouDietProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouDietProject
+//
+//  Created by fukutappe on 2019/12/10.
+//  Copyright © 2019 福田卓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
